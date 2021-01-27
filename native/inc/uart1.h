b8738bc031bc12c8103538cf12dd51b89602c65f
qitas <qitas@qitas.cn>
2021-01-27 14:58:34 +0800

    fix

--- native/inc/uart1.h
+++ native/inc/uart1.h
@@ -8,6 +8,6 @@ uint8_t uart1_put(const char *s);
 uint8_t uart1_get(void);
 void uart1_set(uint8_t data);
 
-void uart1_isr(void) __interrupt(18);
+// void uart1_isr(void) __interrupt(18);
 
 #endif /* __UART1_H */
