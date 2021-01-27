1c14dbe15d963cd2d9f1e9b902638f38f3e66373
qitas <qitas@qitas.cn>
2021-01-27 14:54:32 +0800

    ADD

--- native/inc/pin.h
+++ native/inc/pin.h
@@ -1,7 +1,14 @@
 #ifndef __PIN_H
 #define __PIN_H
 
+#include "stm8s.h"
 // #include <stdint.h>
+#define LED1_PORT               (GPIOB)
+#define LED1_PIN                (GPIO_PIN_5)
+#define LED1_ON                 (LED1_PORT->ODR &= (uint8_t)(~LED1_PIN))
+#define LED1_OFF                (LED1_PORT->ODR |= (uint8_t)LED1_PIN)
+#define LED1_TOG                (LED1_PORT->ODR ^= (uint8_t)LED1_PIN)
+
 
 void pin_init(void);
 void irq_init(void);
